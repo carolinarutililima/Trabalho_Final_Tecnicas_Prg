0afa25343f25b4ec7cf88aad656da1177bafd5cb
Gabriel Del <gabrieldelc.barros@gmail.com>
2024-02-25 15:26:57 -0300

    print formated table

--- gabriel.py
+++ gabriel.py
@@ -1,13 +1,22 @@
 import pandas as pd
 import numpy as np
 
+# Objetivo: Média de preços de compra por consumidores de um mesmo estado
+# order_id, customer_id, customer_state, customer_city, payment_value
+
 customers = pd.read_csv("datasets/olist_customers_dataset.csv")
 orders = pd.read_csv("datasets/olist_orders_dataset.csv")
 payment = pd.read_csv("datasets/olist_order_payments_dataset.csv")
-
 # print(customers, orders, payment)
 
-# Objetivo: Média de preços de compra por consumidores de um mesmo estado
-# order_id, customer_id, payment_value, customer_state
+customers_short = customers[['customer_id','customer_city', 'customer_state']]
+orders_short = orders[['customer_id','order_id']]
+payment_short = payment[['order_id', 'payment_value']]
+# print(customers_short, orders_short, payment_short)
 
-print(customers.isnull().sum().sum())
+df = pd.merge(customers_short, orders_short, on="customer_id", how="inner")
+df = pd.merge(df, payment_short, on="order_id", how="inner")
+df = df[['order_id', 'customer_id', 'customer_state', 'customer_city', 'payment_value']]
+print(df)
+
+# print(df.isnull().sum().sum())
